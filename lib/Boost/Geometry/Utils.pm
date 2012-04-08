a7c5fb071f614c8ce46b97c5db826303bae8b733
Alessandro Ranellucci <aar@cpan.org>
2012-04-09 00:20:51 +0200

    Initial commit

---
+++ lib/Boost/Geometry/Utils.pm
@@ -0,0 +1,104 @@
+package Boost::Geometry::Utils;
+# ABSTRACT: Bindings for the Boost Geometry library
+use strict;
+use warnings;
+
+require Exporter;
+our @ISA = qw(Exporter);
+our $VERSION;
+
+BEGIN {
+    use XSLoader;
+    $VERSION = '0.01';
+    XSLoader::load('Boost::Geometry::Utils', $VERSION);
+}
+
+our @EXPORT_OK = qw(polygon_to_wkt linestring_to_wkt wkt_to_multilinestring
+    polygon linestring
+    polygon_linestring_intersection);
+
+sub polygon_to_wkt {
+    sprintf 'POLYGON(%s)', join ',', map { sprintf '(%s)', join ',', map { join ' ', @$_ } @$_ } @_;
+}
+
+sub linestring_to_wkt {
+    sprintf 'LINESTRING(%s)', join ',', map { join ' ', @$_ } @{$_[0]};
+}
+
+sub wkt_to_multilinestring {
+    return [] if $_[0] eq 'MULTILINESTRING()';
+    $_[0] =~ s/^MULTILINESTRING\(\(//;
+    $_[0] =~ s/\)\)$//;
+    [ map [ map [ split / / ], split /,/ ], split /\),\(/, $_[0] ];
+}
+
+sub polygon {
+    _read_wkt_polygon(polygon_to_wkt(@_));
+}
+
+sub linestring {
+    _read_wkt_linestring(linestring_to_wkt(@_));
+}
+
+sub polygon_linestring_intersection {
+    wkt_to_multilinestring(_multilinestring_to_wkt(_polygon_linestring_intersection(@_)));
+}
+
+1;
+
+=head1 SYNOPSIS
+
+    use Boost::Geometry::Utils qw(polygon linestring polygon_linestring_intersection);
+    
+    my $square = [  # ccw
+        [10, 10],
+        [20, 10],
+        [20, 20],
+        [10, 20],
+    ];
+    my $hole_in_square = [  # cw
+        [14, 14],
+        [14, 16],
+        [16, 16],
+        [16, 14],
+    ];
+    my $polygon = polygon($square, $hole_in_square);
+    my $linestring = linestring([ [5, 15], [30, 15] ]);
+    
+    my $intersection = polygon_linestring_intersection($polygon, $linestring);
+    
+    # $intersection is:
+    # [
+    #     [ [10, 15], [14, 15] ],
+    #     [ [16, 15], [20, 15] ],
+    # ]
+
+=head1 ABSTRACT
+
+This module provides bindings to perform some geometric operations using
+the Boost Geometry library. It does not aim at providing full bindings
+for such library, and that's why I left the I<Boost::Geometry> namespace
+free. I'm unsure about the optimal architectural for providing full 
+bindings, but I'm interested in such a project -- so, if you have ideas
+please get in touch with me.
+
+=head1 METHODS
+
+=head2 polygon
+
+Converts one or more arrayref(s) of points to a Boost Geometry polygon 
+structure. Note that a Boost Geometry polygon is actually a polygon
+with holes.
+
+=head2 linestring
+
+Converts an arrayref of points to a Boost Geometry linestring.
+
+=head2 polygon_linestring_intersection
+
+Performs an intersection between the supplied polygon and linestring,
+and returns an arrayref of linestrings (represented as arrayrefs of
+points).
+Note that such an intersection is also called I<clipping>.
+
+=cut
---
+++ src/myinit.h
@@ -0,0 +1,10 @@
+#include <boost/geometry.hpp>
+#include <boost/geometry/geometries/point_xy.hpp>
+#include <boost/geometry/geometries/polygon.hpp>
+#include <boost/geometry/geometries/linestring.hpp>
+#include <boost/geometry/multi/geometries/multi_linestring.hpp>
+#include <boost/geometry/io/wkt/wkt.hpp>
+
+typedef boost::geometry::model::polygon<boost::geometry::model::d2::point_xy<double>,false,false> polygon;
+typedef boost::geometry::model::linestring<boost::geometry::model::d2::point_xy<double> > linestring;
+typedef boost::geometry::model::multi_linestring<linestring> multilinestring;
